2ccfa4eca0d063f213d355c9ca575ac76fca78b2
FelipeTorres <manfeltor@live.com>
2023-08-04 16:44:30 -0300

    no mucho avance :/ pero se logro entender mejor como tratar los sabados

--- testing.py
+++ testing.py
@@ -4,5 +4,13 @@ import pandas as pd, re
 
 prbRe = re.compile(r"(\d{1,2}[.:]\d{1,2}|\d{1,4}) [AayY] (\d{1,2}[.:]\d{1,2}|\d{1,4})")  # codigo Regex
 def hourleek(df: pd.DataFrame, column: str) -> pd.DataFrame:
-    matches = df[column].str.extract(prbRe, expand=True)
-    new_column_names = [f"{column_name}_group{i}" for i in range(1, len(matches.columns) + 1)]
+    raw = df[column]
+# de lo que estoy deduciendo del original, tengo que separar las columnas en las cuales este contenido el sabado
+# y separarlas del resto del rex, sera viable?
+    for i in raw:
+        if "sab" in i.lower():
+            i = i[:i.lower().index("sab")]
+            j = i[i.lower().index("sab"):]
+        if "dom" in i.lower():
+            i = i[:i.lower().index("dom")]
+            j = i[i.lower().index("dom"):]
