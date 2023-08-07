504b50ba1e79eb8a6c905af5d1ed4e93574d4b11
FelipeTorres <manfeltor@live.com>
2023-08-07 15:31:27 -0300

    llegando a un mejor entendimiento, tratando de llenar el dic a pasar a DF con logicas, save game mas que todo

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #! python3
+import testing
+import imp1
 
+def rall(fpth: str, col: str):
+    df = imp1.openfile(fpth)
+    testing.hourleek(df, col)
+
+
+a = rall(r"C:\Users\ftorres\Documents\py\test.xlsx", "observaciones")
--- testing.py
+++ testing.py
@@ -2,15 +2,29 @@
 
 import pandas as pd, re
 
-prbRe = re.compile(r"(\d{1,2}[.:]\d{1,2}|\d{1,4}) [AayY] (\d{1,2}[.:]\d{1,2}|\d{1,4})")  # codigo Regex
+
 def hourleek(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    prbRe = re.compile(r"(\d{1,2}[.:]\d{1,2}|\d{1,4}) [AayY] (\d{1,2}[.:]\d{1,2}|\d{1,4})")  # codigo Regex
     raw = df[column]
+    dic1 = {
+        "lav_11": [],
+        "lav_12": [],
+        "lav_21": [],
+        "lav_22": [],
+        "sab_11": [],
+        "sab_12": []
+    }
 # de lo que estoy deduciendo del original, tengo que separar las columnas en las cuales este contenido el sabado
 # y separarlas del resto del rex, sera viable?
     for i in raw:
-        if "sab" in i.lower():
-            i = i[:i.lower().index("sab")]
-            j = i[i.lower().index("sab"):]
-        if "dom" in i.lower():
-            i = i[:i.lower().index("dom")]
-            j = i[i.lower().index("dom"):]
+        i = str(i)
+        mtch1 = prbRe.search(i)
+        if mtch1 == None:
+            dic1["lav_11"].append("sin match")
+            dic1["lav_12"].append("sin match")
+            dic1["lav_21"].append("sin match")
+            dic1["lav_22"].append("sin match")
+            dic1["sab_11"].append("sin match")
+            dic1["sab_12"].append("sin match")
+        else:
+            if "sab" in i:
